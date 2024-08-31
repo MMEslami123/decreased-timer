08b7267fb87368080778163a00b9c34710ec8519
M.M.Eslami <130648329+MMEslami123@users.noreply.github.com>
2024-08-31 20:13:39 +0330

    show the number in screen when you start timer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const circle = document.querySelector("circle");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
-
+const counter = document.querySelector("p");
 
 button.addEventListener("click", () => {
   if (input.value == "") {
@@ -9,6 +9,11 @@ button.addEventListener("click", () => {
   } else {
     button.classList.toggle("active");
     let timeCount = input.value;
+    counter.style.display = "block";
+    counter.innerHTML = timeCount;
+    setInterval(() => {
+      counter.innerHTML -= 1;
+    }, 1000);
     circle.style.animation = `timer ${timeCount}s linear backwards`;
     input.style.display = "none";
     button.innerHTML = "Stop";
